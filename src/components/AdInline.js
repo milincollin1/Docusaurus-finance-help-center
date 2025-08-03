1d70a48979962cde6c6ff934e693e0239f2942df
milincollin1 <e.buechner1@gmail.com>
2025-08-03 12:41:39 -0400

    Add placeholder AdInline component

---
+++ src/components/AdInline.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function AdInline() {
+  return (
+    <div
+      style={{
+        border: '1px solid #ccc',
+        padding: '1rem',
+        margin: '2rem 0',
+        textAlign: 'center',
+        borderRadius: '4px',
+      }}
+    >
+      {/* Replace this with your actual ad markup */}
+      <p><em>Your Ad Here</em></p>
+    </div>
+  );
+}
